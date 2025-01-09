e992334534d2ead6c96041f6fd7168cd11669696
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-09 14:46:35 +0400

    Update ActorServiceImpl.java

--- src/main/java/com/workintech/s19d1/service/ActorServiceImpl.java
+++ src/main/java/com/workintech/s19d1/service/ActorServiceImpl.java
@@ -1,4 +1,43 @@
 package com.workintech.s19d1.service;
 
-public class ActorServiceImpl {
+import com.workintech.s19d1.entity.Actor;
+import com.workintech.s19d1.repository.ActorRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Data
+public class ActorServiceImpl implements ActorService {
+    private ActorRepository actorRepository;
+
+    @Override
+    public List<Actor> findAll() {
+        return actorRepository.findAll();
+    }
+
+    @Override
+    public Actor findById(Long id) {
+        return actorRepository.findById(id).orElseThrow(() -> new ActorException("Actor does not exist with given id " + id, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public Actor save(Actor actor) {
+        return actorRepository.save(actor);
+    }
+
+    @Override
+    public Actor update(Long id, Actor actor) {
+        Actor actor1 = findById(id);
+        return actorRepository.save(actor);
+    }
+
+    @Override
+    public void delete(Actor actor) {
+        actorRepository.delete(actor);
+    }
 }
