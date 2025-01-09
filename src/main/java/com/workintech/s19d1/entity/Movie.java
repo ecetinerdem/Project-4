b40d677909c2b87e6eb27a1d5d88c7a95c88eecf
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-09 13:31:51 +0400

    Update Movie.java

--- src/main/java/com/workintech/s19d1/entity/Movie.java
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -1,14 +1,33 @@
 package com.workintech.s19d1.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @Entity
 @Table(name = "movie", schema = "cinema")
 @Data
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "director_name")
+    private String directorName;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+
 }
