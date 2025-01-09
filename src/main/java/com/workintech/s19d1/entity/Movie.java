54d9876b781a4487ec60cf90480318357940da33
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-09 13:41:38 +0400

    Update Movie.java

--- src/main/java/com/workintech/s19d1/entity/Movie.java
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -1,11 +1,14 @@
 package com.workintech.s19d1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Entity
@@ -18,9 +21,11 @@ public class Movie {
     private Long id;
 
     @Column(name = "name")
+    @NotNull(message ="Movie name cannot be null")
     private String name;
 
     @Column(name = "director_name")
+    @NotNull(message ="Director name cannot be null")
     private String directorName;
 
     @Column(name = "rating")
@@ -29,5 +34,15 @@ public class Movie {
     @Column(name = "release_date")
     private LocalDate releaseDate;
 
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "movie_actor", schema = "cinema", joinColumns = @JoinColumn(name = "movie_id"), inverseJoinColumns = @JoinColumn(name = "actor_id"))
+    private List<Actor> actors;
+
+    public void addActor(Actor actor) {
+        if(actors == null) {
+            actors = new ArrayList<>();
+        }
+        actors.add(actor);
+    }
 
 }
