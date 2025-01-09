a6958af6254f4c7bc251ece17a917ed63b841570
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-09 13:33:32 +0400

    Update Actor.java

--- src/main/java/com/workintech/s19d1/entity/Actor.java
+++ src/main/java/com/workintech/s19d1/entity/Actor.java
@@ -1,4 +1,20 @@
 package com.workintech.s19d1.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "actor", schema = "cinema")
 public class Actor {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private LocalDate birthDate;
 }
