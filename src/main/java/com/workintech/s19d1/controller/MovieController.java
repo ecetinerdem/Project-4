4ee39f1c095a16a003e7bbdb8742999510cf6310
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-10 11:51:17 +0400

    Update MovieController.java

--- src/main/java/com/workintech/s19d1/controller/MovieController.java
+++ src/main/java/com/workintech/s19d1/controller/MovieController.java
@@ -1,11 +1,25 @@
 package com.workintech.s19d1.controller;
 
+import com.workintech.s19d1.dto.MovieRequest;
+import com.workintech.s19d1.dto.MovieResponse;
+import com.workintech.s19d1.entity.Movie;
+import com.workintech.s19d1.service.MovieService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
 @AllArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
+
+    @GetMapping
+    public List<MovieResponse> findAll() {
+        List<Movie> movies = movieService.findAll();
+        return Converter.movieResponseConvert(movies);
+    }
 }
