948865528349ea848232b763af552198288dad89
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-10 12:07:17 +0400

    Update MovieController.java

--- src/main/java/com/workintech/s19d1/controller/MovieController.java
+++ src/main/java/com/workintech/s19d1/controller/MovieController.java
@@ -2,12 +2,14 @@ package com.workintech.s19d1.controller;
 
 import com.workintech.s19d1.dto.MovieRequest;
 import com.workintech.s19d1.dto.MovieResponse;
+import com.workintech.s19d1.entity.Actor;
 import com.workintech.s19d1.entity.Movie;
 import com.workintech.s19d1.service.MovieService;
+import com.workintech.s19d1.util.Converter;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +24,43 @@ public class MovieController {
         List<Movie> movies = movieService.findAll();
         return Converter.movieResponseConvert(movies);
     }
+
+    @GetMapping("/{id}")
+    public MovieResponse findById(@PathVariable("id") Long id) {
+        Movie foundMovie = movieService.findById(id);
+        return Converter.movieResponseConvert(foundMovie);
+    }
+
+    @Transactional
+    @PostMapping
+    public MovieResponse save(@Validated @RequestBody MovieRequest movieRequest) {
+        Movie foundMovie = movieRequest.getMovie();
+        List<Actor> actors = movieRequest.getActors();
+
+        for(Actor actor: actors) {
+            foundMovie.addActor(actor);
+        }
+        Movie savedMovie = movieService.save(foundMovie);
+        return Converter.movieResponseConvert(savedMovie);
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public MovieResponse update(@PathVariable("id") Long id, @Validated @RequestBody MovieRequest movieRequest) {
+        Movie foundMovie = movieService.findById(id);
+        Movie toBeChanged = movieRequest.getMovie();
+        toBeChanged.setActors(foundMovie.getActors());
+        toBeChanged.setId(foundMovie.getId());
+        movieService.save(toBeChanged);
+        return Converter.movieResponseConvert(toBeChanged);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public MovieResponse delete(@PathVariable("id") Long id) {
+        Movie toBeDeleted = movieService.findById(id);
+        movieService.delete(toBeDeleted);
+        return Converter.movieResponseConvert(toBeDeleted);
+    }
+
 }
