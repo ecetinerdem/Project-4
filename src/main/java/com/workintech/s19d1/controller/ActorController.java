f1abd31c526167f3781d17bae4426c3c785e2430
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-10 11:27:00 +0400

    Update ActorController.java

--- src/main/java/com/workintech/s19d1/controller/ActorController.java
+++ src/main/java/com/workintech/s19d1/controller/ActorController.java
@@ -1,11 +1,26 @@
 package com.workintech.s19d1.controller;
 
+import com.workintech.s19d1.dto.ActorRequest;
+import com.workintech.s19d1.dto.ActorResponse;
+import com.workintech.s19d1.entity.Actor;
+import com.workintech.s19d1.service.ActorService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/actor")
 public class ActorController {
+
+    private  final ActorService actorService;
+
+    @GetMapping
+    public List<ActorResponse> findAll(ActorRequest actorRequest) {
+        List<Actor> actors = actorService.findAll();
+        return Converter.actorResponseConverter(actors);
+    }
 }
