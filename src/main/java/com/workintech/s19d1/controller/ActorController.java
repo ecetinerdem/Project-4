1d5b7ed4281631389c5f2746befadb908ffe8c32
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-10 11:22:47 +0400

    Update ActorController.java

--- src/main/java/com/workintech/s19d1/controller/ActorController.java
+++ src/main/java/com/workintech/s19d1/controller/ActorController.java
@@ -1,4 +1,11 @@
 package com.workintech.s19d1.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/actor")
 public class ActorController {
 }
