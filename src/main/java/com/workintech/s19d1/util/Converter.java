3f7d67b43320f35301fc39d6462304b830063193
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-10 11:31:39 +0400

    Create Converter.java

---
+++ src/main/java/com/workintech/s19d1/util/Converter.java
@@ -0,0 +1,24 @@
+package com.workintech.s19d1.util;
+
+import com.workintech.s19d1.dto.ActorResponse;
+import com.workintech.s19d1.entity.Actor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Converter {
+
+    public List<ActorResponse> actorResponseConvert(List<Actor> actors) {
+        List<ActorResponse> actorResponses = new ArrayList<>();
+        for(Actor actor: actors) {
+            actorResponses.add(new ActorResponse(actor.getId(), actor.getFirstName(), actor.getBirthDate(),actor.getMovies()));
+        }
+        return actorResponses;
+    }
+}
